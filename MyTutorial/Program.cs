7d062ea410c5d0a6615048a1c35ab2325e01b50e
janicijevesovic <janicije95@hotmail.com>
2024-09-09 20:23:53 +0200

    Feat: Initial Commit

---
+++ MyTutorial/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace MyTutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int age = 23;
+            Console.ReadLine();
+        }
+    }
+}
